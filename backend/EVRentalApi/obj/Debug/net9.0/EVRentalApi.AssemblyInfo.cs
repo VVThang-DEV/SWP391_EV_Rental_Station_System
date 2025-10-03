35d6b198549a71be14680b93d8bcac44e6a799ce
ThangVVSE180202 <ThangVVSE180202@fpt.edu.vn>
2025-10-03 14:41:29 +0700

    Revert "Merge branch 'ThangVVSE180202' into dev-new"
    This reverts commit d2d0335701286b3de83f1c7645efcbfae121d197, reversing
    changes made to 3c7c1458368eb7f6a20917913878e517c40123c7.

--- backend/EVRentalApi/obj/Debug/net9.0/EVRentalApi.AssemblyInfo.cs
+++ backend/EVRentalApi/obj/Debug/net9.0/EVRentalApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EVRentalApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+186c34302ea8ecb1441f8deda9296664e151d036")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c741b303095de488fa83dc4755fd9462ceb455d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EVRentalApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EVRentalApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
