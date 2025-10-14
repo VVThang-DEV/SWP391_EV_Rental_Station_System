353456e21280485048f21ec56fdf7ff48ca5ce1b
ducltmse180065 <Ducltmse180065@fpt.edu.vn>
2025-10-14 10:33:59 +0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import { TranslationProvider } from "@/contexts/TranslationContext";
 import Navbar from "./components/Navbar";
+
+
 import {
   Index,
   Vehicles,
