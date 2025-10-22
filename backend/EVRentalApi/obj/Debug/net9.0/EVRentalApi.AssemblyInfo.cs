68474f9ab77ba5c9ec01308447daa13779986aef
LuTu-2004 <tulpnse185102@fpt.edu.vn>
2025-10-22 16:35:18 +0700

    Fix station management to use API data instead of hardcoded data

--- backend/EVRentalApi/obj/Debug/net9.0/EVRentalApi.AssemblyInfo.cs
+++ backend/EVRentalApi/obj/Debug/net9.0/EVRentalApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EVRentalApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0b2a351a14f3941c61393b57680dd2417bb48eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8a72905f4c6df931ab43da2fda95df86bf23a98")]
 [assembly: System.Reflection.AssemblyProductAttribute("EVRentalApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EVRentalApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
