20989fffd64596e9dccf57785db1db67682c2867
Nguyen Song <songntse171665@fpt.edu.vn>
2025-11-05 17:06:22 +0700

    fix lỗi incident

--- backend/EVRentalApi/obj/Debug/net9.0/EVRentalApi.AssemblyInfo.cs
+++ backend/EVRentalApi/obj/Debug/net9.0/EVRentalApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EVRentalApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf459b47795e951f19a0da512b1e251b7816cc2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7195dbe6c281f3e28e0491d5eb3b62694e477c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EVRentalApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EVRentalApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
